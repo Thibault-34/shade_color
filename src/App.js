8078cbea305abe1f27df38cfdf122ce2ddace756
Thibault Capelli <capellithibault@gmail.com>
2020-03-13 11:51:00 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { LightenDarkenColor } from './utils/edit_colors';
+
+const ColorContainer = styled.div`
+	width: 100px;
+`;
+
+const Color = styled.div`
+	width: 100px;
+	height: 100px;
+	background-color: ${({ color }) => color};
+`;
+
+const Input = styled.input`
+	box-sizing: border-box;
+	border: grey solid 1px;
+	width: 100%;
+	text-align: center;
+	margin: 3px 0;
+`;
+
+class App extends React.Component {
+	state = {
+		color: '#000000',
+		degree: 0,
+	};
+
+	_onChange = (key, e) => {
+		this.setState({ [key]: e.target.value });
+	};
+
+	render() {
+		const { color, degree } = this.state;
+		const newColor = LightenDarkenColor(color, degree);
+
+		return (
+			<div
+				style={{
+					display: 'flex',
+					alignItems: 'center',
+					justifyContent: 'space-around',
+					width: '100vw',
+					height: '100vh',
+					boxSizing: 'border-box',
+					padding: '20%',
+				}}>
+				<ColorContainer>
+					<Input
+						type="text"
+						value={color}
+						onChange={e => this._onChange('color', e)}
+					/>
+					<Color color={color} />
+				</ColorContainer>
+
+				<div
+					style={{
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+					}}>
+					<div
+						style={{
+							fontSize: 13,
+							textTransform: 'uppercase',
+							fontWeight: 'bold',
+						}}>
+						Degré
+					</div>
+					<hr style={{ width: '100%' }} />
+					<Input
+						type="text"
+						value={degree}
+						onChange={e => this._onChange('degree', e)}
+					/>
+					<div style={{ fontSize: 12, color: 'grey' }}>
+						<b>Nombre entier</b>; <b>Assombrir</b>: négatif;
+						<b>Eclaircir</b>: positif;
+					</div>
+				</div>
+
+				<ColorContainer>
+					<Input type="text" value={newColor} />
+					<Color color={newColor} />
+				</ColorContainer>
+			</div>
+		);
+	}
 }
 
 export default App;
